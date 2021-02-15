f3cb073456a12c21ba6cae61b23d6aa7ca5333fc
Simon Engblom <simonhagridbloons@gmail.com>
2021-02-15 15:04:26 +0100

    First Commit

---
+++ src/Cirkelklass.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Simon
+ */
+public class Cirkelklass {
+
+    private int x;
+    private int y;
+    private int r;
+
+    public Cirkelklass(int x, int y, int r) {
+        this.x = x;
+        this.y = y;
+        if (r > 0) {
+            this.r = r;
+        } else {
+            this.r = -r;
+        }
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getR() {
+        return r;
+    }
+
+    public void setR(int r) {
+        this.r = r;
+    }
+    
+    
+    
+}
